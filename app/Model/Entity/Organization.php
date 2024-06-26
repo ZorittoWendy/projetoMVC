364e550c5c32b8ba6041e451657d6b2e87230c32
ZorittoWendy <zorittoprogramador@gmail.com>
2024-06-25 21:44:07 -0300

    Inicio de um projeto MVC em PHP

---
+++ app/Model/Entity/Organization.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Model\Entity;
+
+class Organization{
+
+  public $id = 1;
+
+  public $name = 'Zoritto Dev';
+
+  public $site = 'www.linkedin.com/in/wendeson-farias';
+
+  public $description = 'Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
+   enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation
+   amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet 
+   voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. 
+   Sit irure elit esse ea nulla sunt ex occaecat.';
+
+  
+
+}
